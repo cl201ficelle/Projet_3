41de7ed735db02df14bf0dee01c20ea994760c5c
Melanie C <melanie.chedhomme96@gmail.com>
2023-08-23 14:12:57 +0200

    ajout ligne recuperation mdp js

--- FrontEnd/connectionpage.js
+++ FrontEnd/connectionpage.js
@@ -1,6 +1,11 @@
+
+// récupération des éléments entré par utilisateur
 let baliseEmail = document.getElementById("email")
 let email = baliseEmail.value
-// console.log(email); // affiche ce qui est contenu dans la balise name
+
+let balisePassword = document.getElementById("password")
+let password = balisePassword.value
+// console.log(email); // affiche ce qui est contenu dans la balise 
 
 // erreur quand j'essaye de récupérer login de API
 const res = await fetch("http://localhost:5678/api/works")
