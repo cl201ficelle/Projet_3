d317fa87f19d583e7d462a5f6db5f0901479621c
Melanie C <melanie.chedhomme96@gmail.com>
2023-08-25 10:30:36 +0200

    recreation button filtre a partir variable globale

--- FrontEnd/filtre.js
+++ FrontEnd/filtre.js
@@ -31,60 +31,45 @@ let allCategories = JSON.parse(localStorage.getItem("mydata2"))
 
 // export/import ne fonctionne pas mais mes données de la fonction getAllWorks sont bien la donc pour l'instant je continue
 
-promise2 = fetch("http://localhost:5678/api/categories")
-// deux then car promise
-promise2
-.then((response) => {
+function buttonFiltreTous(){ 
+     // creation bouton Tous car pas dans Api
+    let buttonFiltreTous = document.createElement("button")
+    // je met dans le bouton le texte qu'il doit contenir
+    buttonFiltreTous.innerText = "Tous"
+    divFiltres.appendChild(buttonFiltreTous)
+    // ajout event listener pour changer couleur au click
+    buttonFiltreTous.addEventListener("click", () => {
+    //background vert/écriture blanc
+    buttonFiltreTous.style.backgroundColor = "#1D6154"
+    buttonFiltreTous.style.color = "white"
+    })}
+
+buttonFiltreTous()
    
-    const promise3 =  response.json();
 
-    promise3.then((categories) => {
+function buttonFiltresAll (){
+    // //    boucle for pour récupérer toutes les catégories i
+    // // mise en place du compteur : initialisation de i, pour i < longueur catégories, on incrémente i
+    for (let i=0; i<allCategories.length; i++){
+        // création bouton autant de bouton qu'il y a de catégorie dans la section catégories de l'api
+        const buttonFiltre = document.createElement("button") 
+        // je donne au bouton le nom des catégories
+        buttonFiltre.innerText = allCategories[i].name;
+        // je mets ces boutons dans la div que je veux
+        divFiltres.appendChild(buttonFiltre)
+         // j'ajoute les changements de style au moment du click
+         buttonFiltre.addEventListener("click", () => {
+        //background vert
+        buttonFiltre.style.backgroundColor = "#1D6154"
+        buttonFiltre.style.color = "white"
+        })
+    }
+}
+
+buttonFiltresAll()
+    
+
+ 
         
-        // creation bouton Tous car pas dans Api
-        let buttonFiltreTous = document.createElement("button")
-        // je met dans le bouton le texte qu'il doit contenir
-            buttonFiltreTous.innerText = "Tous"
-            divFiltres.appendChild(buttonFiltreTous)
-            // ajout event listener pour changer couleur au click
-            buttonFiltreTous.addEventListener("click", () => {
-            //background vert/écriture blanc
-            buttonFiltreTous.style.backgroundColor = "#1D6154"
-            buttonFiltreTous.style.color = "white"
-            
-        });
-                
-
-
-            
-    //    boucle for pour récupérer toutes les catégories i
-    // mise en place du compteur : initialisation de i, pour i < longueur catégories, on incrémente i
-        for (let i= 0; i<categories.length; i++){
-            // création bouton autant de bouton qu'il y a de catégorie dans la section catégories de l'api
-            const buttonFiltre = document.createElement("button")
-            // je donne au bouton le nom des catégories
-            buttonFiltre.innerText = categories[i].name;
-            // je mets ces boutons dans la div que je veux
-            divFiltres.appendChild(buttonFiltre)
-            // j'ajoute les changements de style au moment du click
-            buttonFiltre.addEventListener("click", () => {
-                //background vert 
-                buttonFiltre.style.backgroundColor = "#1D6154"
-                buttonFiltre.style.color = "white"
-                 // admettons que j'ai réussi à récupérer les deux fetch categories et allWorks : ajout fonction filtre
-            // const elementFiltres = allWorks.filter(function(allWorks, categories){
-                // return allWorks.name === categories.name
-            // })   
-                });
-
-            
-             }
-              
-              
-
-            
-    })
-        
-
-    });
 
    
