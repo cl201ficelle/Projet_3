38b0361ccffcd9fa99ecdb79b1f65550b2e8dcc8
Melanie C <melanie.chedhomme96@gmail.com>
2023-08-25 10:32:26 +0200

    seulement script js connection dans html

--- FrontEnd/connectionpage.js
+++ FrontEnd/connectionpage.js
@@ -7,6 +7,6 @@ let balisePassword = document.getElementById("password")
 let password = balisePassword.value
 // console.log(email); // affiche ce qui est contenu dans la balise 
 
-// erreur quand j'essaye de récupérer login de API
-const res = await fetch("http://localhost:5678/api/works")
-console.log(res)
+// // erreur quand j'essaye de récupérer login de API
+// const res = await fetch("http://localhost:5678/api/works")
+// console.log(res)
