7c01b1aa91d0902f31df0c7bf9883686b11e8450
Melanie C <melanie.chedhomme96@gmail.com>
2023-08-27 19:49:37 +0200

    filtre fonctionnel et essai page connection

--- FrontEnd/connectionpage.js
+++ FrontEnd/connectionpage.js
@@ -1,22 +1,60 @@
+// pour ne pas que la page se rafraichit au submit
+document.getElementById("login").addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+// récupération des values que l'utilisateur entre dans les input
+let userEmailInput = document.getElementById("email");
+let userEmailValue = userEmailInput.value;
+  
+let userPasswordInput = document.getElementById("password");
+let userPasswordValue = userPasswordInput.value;
 
-// récupération des éléments entré par utilisateur
-let baliseEmail = document.getElementById("email")
-let email = baliseEmail.value
-
-let balisePassword = document.getElementById("password")
-let password = balisePassword.value
-// console.log(email); // affiche ce qui est contenu dans la balise 
-
-async function connect (){
-    params ={
-        "email": "sophie.bluel@test.tld",
-        "password": "S0phie"
+let Log = document.getElementById("Log")
+console.log(Log.textContent)
+    login(userEmailValue, userPasswordValue);
+  
+  
+  function login(username, password) {
+    // charge utile body
+    let params = {
+    email: username,
+    password: password
     }
-    const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        body:JSON.stringify(params)
-    })
-    console.table(response)
     
-
+    fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+      },
+        body: JSON.stringify(params)
+    })
+      .then(res => res.json())
+      .then(data=> {
+  
+        if (data.token) {
+            alert("Connexion réussie");
+            sessionStorage.setItem("token", data.token);
+        //   redirection accueil
+            window.location.href = 'index.html'; 
+        } else {
+          throw new Error("Une erreur s’est glissée dans votre adresse e-mail ou votre mot de passe"); 
+        }
+      })
+      .catch((error)=> {
+         alert(error.message); 
+      })
+  }
+})
+  
+// if user click sur #Log quand il y a token dans local storage alors cette fonction est activée
+function LogOut (){
+    localStorage.removeItem("token")
+}
+function getToken(){
+    return localStorage.getItem("token")
+}
+// si le token est différent de null alors le text de #log est logout
+function isConnected(){
+    return getToken() !== '',
+    Log.innerText="logout"
 }
--- FrontEnd/filtre.js
+++ FrontEnd/filtre.js
@@ -1,62 +1,62 @@
-// import {getAllWorks} from "gallery.js";
-// getAllWorks()
-// let allWorks = JSON.parse(localStorage.getItem("mydata"))
+// // import {getAllWorks} from "gallery.js";
+// // getAllWorks()
+// // let allWorks = JSON.parse(localStorage.getItem("mydata"))
 
-//  console.log(allWorks.length)
-
-
-
-// Récupération section portfolio
-const portfolio = document.getElementById("portfolio")
-let gallery = document.querySelector(".gallery")
-// console.log(portfolio)
-// création div pour accueillir les filtres
-const divFiltres = document.createElement("div")
-// ajout class à ma div
-divFiltres.setAttribute("class", "divFiltres");
-// on met cette div dans section portfolio, j'utilise insertBefore pour que la div se mette au dessus de gallery
-portfolio.insertBefore(divFiltres,gallery)
-
-// recuperation categorieID de chaque travaux
-// function getAllWorksCategoryID {
-// const categoryID = allWorks.categoryID
-// console.log(categoryID)}
-
-//  obtenir toutes les catégories variable globale
-async function getAllCategories (){
-    let response = await fetch ("http://localhost:5678/api/categories")
-    let allCategories = await response.json()
-    // mettre dans local storage 
-    localStorage.setItem("mydata2", JSON.stringify(allCategories))  
-}
+// //  console.log(allWorks.length)
 
 
-getAllCategories()
-let allCategories = JSON.parse(localStorage.getItem("mydata2"))
 
-// export/import ne fonctionne pas mais mes données de la fonction getAllWorks sont bien la donc pour l'instant je continue
+// // Récupération section portfolio
+// const portfolio = document.getElementById("portfolio")
+// let gallery = document.querySelector(".gallery")
+// // console.log(portfolio)
+// // création div pour accueillir les filtres
+// const divFiltres = document.createElement("div")
+// // ajout class à ma div
+// divFiltres.setAttribute("class", "divFiltres");
+// // on met cette div dans section portfolio, j'utilise insertBefore pour que la div se mette au dessus de gallery
+// portfolio.insertBefore(divFiltres,gallery)
 
-function buttonFiltreTous(){ 
-     // creation bouton Tous car pas dans Api
-    let buttonFiltreTous = document.createElement("button")
-    // je met dans le bouton le texte qu'il doit contenir
-    buttonFiltreTous.innerText = "Tous"
-    divFiltres.appendChild(buttonFiltreTous)
-    // ajout event listener pour changer couleur au click
-    buttonFiltreTous.addEventListener("click", () => {
-    //background vert/écriture blanc
-    buttonFiltreTous.style.backgroundColor = "#1D6154"
-    buttonFiltreTous.style.color = "white"
-    const tousTravaux = allWorks.filter(function(tousTravaux){
-      return tousTravaux
+// // recuperation categorieID de chaque travaux
+// // function getAllWorksCategoryID {
+// // const categoryID = allWorks.categoryID
+// // console.log(categoryID)}
+
+// //  obtenir toutes les catégories variable globale
+// async function getAllCategories (){
+//     let response = await fetch ("http://localhost:5678/api/categories")
+//     let allCategories = await response.json()
+//     // mettre dans local storage 
+//     localStorage.setItem("mydata2", JSON.stringify(allCategories))  
+// }
+
+
+// getAllCategories()
+// let allCategories = JSON.parse(localStorage.getItem("mydata2"))
+
+// // export/import ne fonctionne pas mais mes données de la fonction getAllWorks sont bien la donc pour l'instant je continue
+
+// function buttonFiltreTous(){ 
+//      // creation bouton Tous car pas dans Api
+//     let buttonFiltreTous = document.createElement("button")
+//     // je met dans le bouton le texte qu'il doit contenir
+//     buttonFiltreTous.innerText = "Tous"
+//     divFiltres.appendChild(buttonFiltreTous)
+//     // ajout event listener pour changer couleur au click
+//     buttonFiltreTous.addEventListener("click", () => {
+//     //background vert/écriture blanc
+//     buttonFiltreTous.style.backgroundColor = "#1D6154"
+//     buttonFiltreTous.style.color = "white"
+//     const tousTravaux = allWorks.filter(function(tousTravaux){
+//       return tousTravaux
        
-    })
-    document.querySelector(".gallery").innerHTML = ""
-    genererGallery(tousTravaux)
-    console.log(tousTravaux) 
-    })}
+//     })
+//     document.querySelector(".gallery").innerHTML = ""
+//     genererGallery(tousTravaux)
+//     console.log(tousTravaux) 
+//     })}
 
-buttonFiltreTous()
+// buttonFiltreTous()
    
 
 
@@ -75,35 +75,35 @@ buttonFiltreTous()
 
 
 
-function buttonFiltresAll (){
-    // //    boucle for pour récupérer toutes les catégories i
-    // // mise en place du compteur : initialisation de i, pour i < longueur catégories, on incrémente i
-    for (let i=0; i<allCategories.length; i++){
-        // création bouton autant de bouton qu'il y a de catégorie dans la section catégories de l'api
-        const buttonFiltre = document.createElement("button") 
-        // je donne au bouton le nom des catégories
-        buttonFiltre.innerText = allCategories[i].name;
-        // je mets ces boutons dans la div que je veux
-        divFiltres.appendChild(buttonFiltre)
-        console.log(buttonFiltre.textContent)
-         // j'ajoute les changements de style au moment du click
-         buttonFiltre.addEventListener("click", () => {
-        //background vert
-        buttonFiltre.style.backgroundColor = "#1D6154"
-        buttonFiltre.style.color = "white"
-        const worksByCategory = allWorks.filter(function(allWorks){
-        return allWorks.category.name === buttonFiltre.textContent
+// function buttonFiltresAll (){
+//     // //    boucle for pour récupérer toutes les catégories i
+//     // // mise en place du compteur : initialisation de i, pour i < longueur catégories, on incrémente i
+//     for (let i=0; i<allCategories.length; i++){
+//         // création bouton autant de bouton qu'il y a de catégorie dans la section catégories de l'api
+//         const buttonFiltre = document.createElement("button") 
+//         // je donne au bouton le nom des catégories
+//         buttonFiltre.innerText = allCategories[i].name;
+//         // je mets ces boutons dans la div que je veux
+//         divFiltres.appendChild(buttonFiltre)
+//         console.log(buttonFiltre.textContent)
+//          // j'ajoute les changements de style au moment du click
+//          buttonFiltre.addEventListener("click", () => {
+//         //background vert
+//         buttonFiltre.style.backgroundColor = "#1D6154"
+//         buttonFiltre.style.color = "white"
+//         const worksByCategory = allWorks.filter(function(allWorks){
+//         return allWorks.category.name === buttonFiltre.textContent
              
-          })
-        document.querySelector(".gallery").innerHTML = ""
-        genererGallery(worksByCategory)
-        console.log(worksByCategory)
-        })
-    }
-}
+//           })
+//         document.querySelector(".gallery").innerHTML = ""
+//         genererGallery(worksByCategory)
+//         console.log(worksByCategory)
+//         })
+//     }
+// }
 
-buttonFiltresAll()
+// buttonFiltresAll()
     
 
-console.table(allCategories)
-console.log(allWorks)
+// console.table(allCategories)
+// console.log(allWorks)
