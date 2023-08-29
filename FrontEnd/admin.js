7964d109f354787af80bfa44c03b76fb1ea1297f
Melanie C <melanie.chedhomme96@gmail.com>
2023-08-29 14:26:14 +0200

    admin fonctionnel

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -5,24 +5,26 @@ function LogOut(){
   }
 
 
-console.log("hi")
-// Vérifier si le token est présent dans le localStorage
+// si le token est présent, log affiche logout
+function isConnected(){
 if (localStorage.getItem('token')) {
     console.log('Le localStorage contient un token.');
     Log.innerHTML='logout'
-} else {
-    Log.innerHTML='login'
-
-    console.log('Le localStorage ne contient pas de token.');
+}
 }
 
 
-
-  Log.addEventListener('click', function(){
+// si on clique sur log alors que le token est présent, le token se supprime, si le token est absent alors on est redirigé vers la page de connection
+function deconnectionRedirection(){
+Log.addEventListener('click', function(){
     if (localStorage.getItem("token")){
-      LogOut()
+        LogOut()
       console.log("utilisateur deconnecté")
     }else{
+        Log.innerHTML='login'
         window.location.href = "connectionpage.html"
     }
   })
+}
+  isConnected()
+  deconnectionRedirection()
