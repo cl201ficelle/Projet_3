bd8a911bd949cc415528407cb5f2580eb96b0d72
Melanie C <melanie.chedhomme96@gmail.com>
2023-08-29 11:10:39 +0200

    logout fonctionnel

--- FrontEnd/connectionpage.js
+++ FrontEnd/connectionpage.js
@@ -1,6 +1,6 @@
-// pour ne pas que la page se rafraichit au submit
+// récupération de ce que l'user va écrire dans input
 function recupererInputValue(){
-  let userEmailInput = document.getElementById("email");
+let userEmailInput = document.getElementById("email");
 let userEmailValue = userEmailInput.value;
   
 let userPasswordInput = document.getElementById("password");
@@ -8,8 +8,9 @@ let userPasswordValue = userPasswordInput.value;
 return { userEmailValue, userPasswordValue }
 }
 
+// fonction qui va POST les values des inputs
 function submit(username, password) {
-  // charge utile body
+  // charge utile body swagger
   let params = {
   email: username,
   password: password
@@ -17,6 +18,7 @@ function submit(username, password) {
   
   fetch("http://localhost:5678/api/users/login", {
   method: "POST",
+  // header swagger
   headers: {
       "Content-Type": "application/json"
     },
@@ -24,16 +26,14 @@ function submit(username, password) {
   })
     .then(res => res.json())
     .then(data=> {
-
+// s'il y a un token : c'est que la combinaison mdp email est bonne, donc connexion réussi et on stock le token dans le local storage
       if (data.token) {
           alert("Connexion réussie");
           localStorage.setItem("token", data.token);
-
+          // et login devient logout
+          Log.innerText="logout"
       //   redirection accueil
           // window.location.href = 'index.html'; 
-          Log.innerText="logout"
-    
-
       } else {
         throw new Error("Une erreur s’est glissée dans votre adresse e-mail ou votre mot de passe, veuillez réessayer"); 
       }
@@ -44,38 +44,31 @@ function submit(username, password) {
 }
 
 function login(){
+  // pour ne pas que la page se rafraichit au submit
 document.getElementById("login").addEventListener("submit", function (event) {
     event.preventDefault();
 
 let {userEmailValue, userPasswordValue} = recupererInputValue()
-    submit(userEmailValue, userPasswordValue);
+    submit(userEmailValue, userPasswordValue)
+    if (localStorage.getItem("token") !==''){
+  console.log("utilisateur connecté")
+}
 })}
 
-
-
-// if user click sur #Log quand il y a token dans local storage alors cette fonction est activée
-function LogOut (){
-  localStorage.removeItem("token")
-}
-function getToken(){
-    return localStorage.getItem("token")
-}
-// si le token est différent de null alors le text de #log est logout
-function isConnected(){
-    return getToken() !== '';
-   
-}
-isConnected()
-
-if (localStorage.getItem('token')) {
-  console.log('Le localStorage contient un token.');
-} else {
-  console.log('Le localStorage ne contient pas de token.');
-} 
-
 let Log = document.getElementById("Log")
-console.log(Log.textContent)
-
-
+// quand on clique sur l'élément log (écrit logout quand on est connecté), le token se supprime donc on obtient une déconnection
+function LogOut(){
+  localStorage.removeItem("token")
+  Log.innerText="login"
+}
+Log.addEventListener('click', function(){
+  if (localStorage.getItem("token") !==''){
+    LogOut()
+    console.log("utilisateur deconnecté")
+  }
+})
 
 login()
+
+
+
