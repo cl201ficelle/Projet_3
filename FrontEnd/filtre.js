5a1405e13e1376a2c8b661a3a579dfd6bea217fa
Melanie C <melanie.chedhomme96@gmail.com>
2023-09-07 11:36:42 +0200

    nettoyage code

--- FrontEnd/filtre.js
+++ FrontEnd/filtre.js
@@ -1,38 +1,35 @@
-// Récupération section portfolio
 const portfolio = document.getElementById("portfolio")
 let gallery = document.querySelector(".gallery")
-// console.log(portfolio)
-// création div pour accueillir les filtres
 const divFiltres = document.createElement("div")
-// ajout class à ma div
 divFiltres.setAttribute("class", "divFiltres");
-// on met cette div dans section portfolio, j'utilise insertBefore pour que la div se mette au dessus de gallery
 portfolio.insertBefore(divFiltres, gallery)
 let activeButton = null
 
 //  obtenir toutes les catégories variable globale
-async function getAllCategories() {
-    let response = await fetch("http://localhost:5678/api/categories")
-    let allCategories = await response.json()
-    // mettre dans local storage 
-    localStorage.setItem("mydata2", JSON.stringify(allCategories))
-}
-
-
+function getAllCategories() {
+    fetch("http://localhost:5678/api/categories")
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(allCategories) {
+        localStorage.setItem("mydata2", JSON.stringify(allCategories))
+      })
+      .catch(function(error) {
+        console.error("Error fetching and storing data:", error)
+      })
+  }
 getAllCategories()
 let allCategories = JSON.parse(localStorage.getItem("mydata2"))
 
-
+// création bouton Tous
 function buttonFiltreTous() {
-    // creation bouton Tous car pas dans Api
     let buttonFiltreTous = document.createElement("button")
-    // je met dans le bouton le texte qu'il doit contenir
     buttonFiltreTous.innerText = "Tous"
     divFiltres.appendChild(buttonFiltreTous)
     showTous(buttonFiltreTous)
-    // ajout event listener pour changer couleur au click
 }
 
+// Filtre bouton
 function showTous(buttonFiltreTous) {
     buttonFiltreTous.addEventListener("click", () => {
         // s'il y a un bouton actif on enleve le style au bouton précédemment actif
@@ -55,22 +52,17 @@ function showTous(buttonFiltreTous) {
 
 buttonFiltreTous()
 
-
+// création autres boutons
 function buttonFiltresAll() {
-    // //    boucle for pour récupérer toutes les catégories i
-    // // mise en place du compteur : initialisation de i, pour i < longueur catégories, on incrémente i
     for (let i = 0; i < allCategories.length; i++) {
-        // création bouton autant de bouton qu'il y a de catégorie dans la section catégories de l'api
         const buttonFiltre = document.createElement("button")
-        // je donne au bouton le nom des catégories
         buttonFiltre.innerText = allCategories[i].name
-        // je mets ces boutons dans la div que je veux
         divFiltres.appendChild(buttonFiltre)
-        // console.log(buttonFiltre.textContent)
         showFiltered(buttonFiltre)
     }
 }
 
+// filtre autres boutons
 function showFiltered(buttonFiltre) {
     // j'ajoute les changements de style au moment du click
     buttonFiltre.addEventListener("click", () => {
@@ -80,20 +72,17 @@ function showFiltered(buttonFiltre) {
         }
         // on applique ce style au bouton maintenant actif
         setButtonStyle(buttonFiltre)
-        // Mettez à jour le bouton actif
+        // Mettre à jour le bouton actif
         activeButton = buttonFiltre
         const worksByCategory = allWorks.filter(function(allWorks) {
             return allWorks.category.name === buttonFiltre.textContent
-
         })
         document.querySelector(".gallery").innerHTML = ""
         genererGallery(worksByCategory)
-        // console.log(worksByCategory)
     })
 }
-
-
 buttonFiltresAll()
+
 // style du bouton actif  
 function setButtonStyle(button) {
     button.style.backgroundColor = "#1D6154"
@@ -106,5 +95,3 @@ function removeButtonStyle(button) {
 }
 
 
-// console.table(allCategories)
-// console.table(allWorks)
--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -6,7 +6,7 @@ function deleteGalleryWorks() {
     let gallery = document.querySelector(".gallery")
     gallery.innerHTML = ''
 }
-// deleteGalleryWorks()
+deleteGalleryWorks()
 
 // récupération travaux 
 function getAllWorks() {
@@ -33,25 +33,15 @@ let allWorks = JSON.parse(localStorage.getItem("mydata1"))
 
 // générer galerie avec les travaux récupérés
 function genererGallery(Works) {
-    // mise en place du compteur : initialisation de i, pour i < longueur work, on incrémente i
     for (let i = 0; i < Works.length; i++) {
-        // on récupère la balise div avec classe gallery déjà présente dans html
         let gallery = document.querySelector(".gallery")
-        // on crée une balise figure qui contiendra chaque élément work
         const workElement = document.createElement("figure")
-        // on crée une balise image
         const imgElement = document.createElement("img")
-        // on met la source des images, que je connais grâce console.log(allWorks). on utilise l'indice i, comme ca on va obtenir chaque élémént de la liste de l'api.
         imgElement.src = Works[i].imageUrl
-        // on crée balise ficaption qui contiendra le titre de l'image
         const figCaption = document.createElement("figcaption")
-        // on met le texte de title dans la balise figcaption
         figCaption.innerText = Works[i].title
-        // on met les div workElement dans gallery
         gallery.appendChild(workElement)
-        // on met les images dans chaque div workElement
         workElement.appendChild(imgElement)
-        // on met chaque figcaption dans chaque div workElement
         workElement.appendChild(figCaption)
     }
 }
